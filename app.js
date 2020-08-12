91def7afc5b7e29465198d805313d973e739bc23
adarsh1github1 <adarsh97ada@gmail.com>
2020-08-13 00:30:19 +0530

    finalized the app.js changed var and const as benefit

--- app.js
+++ app.js
@@ -2,25 +2,24 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js"); // we required a module date.js locally 
+
+//console.log(date()); // we are printing whatever we had exported from the date.js page , here we have put the () because the date.js is exporting a function getdate so output will be today's date
 
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-let items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
 app.set('view engine', 'ejs'); // this line should be below ------  const app = express();
 
 app.get("/", function(req, res){
     
-    let today = new Date();
-    let options = {
-        weekday : "long",
-        day : "numeric",
-        month : "long"
-    };
-    let day = today.toLocaleDateString("en-US", options)   //en -us is for english and options to format to our preferance
+    let day = date.getDate(); // gets date from the date module we could also get the day
+    // let day = date.getDay();
+
     res.render("list", {listTitle:day, newListItem:items});
     // ejs template is <%= variable_name%>
     // key has to match with exactly the name used in ejs file
@@ -34,7 +33,7 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
     //console.log(req.body);
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if(req.body.list === "Work"){
         workItems.push(item);
         res.redirect("/work");
