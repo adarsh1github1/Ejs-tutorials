548040ba81783f7c3d3000ba05b94986552af0c6
adarsh1github1 <adarsh97ada@gmail.com>
2020-08-12 23:16:58 +0530

    stylesheets added and new separate work list page created

--- app.js
+++ app.js
@@ -6,19 +6,22 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
-var items = [];
+app.use(express.static("public"));
+
+let items = [];
+let workItems = [];
 app.set('view engine', 'ejs'); // this line should be below ------  const app = express();
 
 app.get("/", function(req, res){
     
-    var today = new Date();
-    var options = {
+    let today = new Date();
+    let options = {
         weekday : "long",
         day : "numeric",
         month : "long"
     };
-    var day = today.toLocaleDateString("en-US", options)   //en -us is for english and options to format to our preferance
-    res.render("list", {kindofday:day, newListItem:items});
+    let day = today.toLocaleDateString("en-US", options)   //en -us is for english and options to format to our preferance
+    res.render("list", {listTitle:day, newListItem:items});
     // ejs template is <%= variable_name%>
     // key has to match with exactly the name used in ejs file
         // this line says we render the page  list.ejs and we pass the day to the rjs file 
@@ -30,11 +33,31 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");// we add the newLsitItem in app.render in the app.get after saving the data from the website we redirect to the home website where the newListItem gets sent
+    //console.log(req.body);
+    let item = req.body.newItem;
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+    
+    // we add the newLsitItem in app.render in the app.get after saving the data from the website we redirect to the home website where the newListItem gets sent
 });
 
+app.get("/work" , function(req , res){
+    res.render("list" , {listTitle:"Work" , newListItem : workItems});
+})
+
+app.post("/work" , function(req , res){
+
+    
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
 
 app.listen(3000, function(){
     console.log("server is running on server 3000");
